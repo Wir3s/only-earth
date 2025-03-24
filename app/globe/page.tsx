4c90cf668af0f0c8d9a329d98473f2a5d12b3a3d
Wir3s <113479857+Wir3s@users.noreply.github.com>
2025-03-24 06:16:35 -0500

    Text to globe page

--- app/globe/page.tsx
+++ app/globe/page.tsx
@@ -5,6 +5,9 @@ import GlobeScene from '../../components/Globe';
 export default function GlobePage() {
   return (
     <main className="h-screen">
+        
+            <h1 className="text-2xl text-center mt-10">Data Coming Soon</h1>
+        
       <GlobeScene />
     </main>
   );
